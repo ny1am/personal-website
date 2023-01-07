f68c5879f0c1acb1582c989f301ee2e83ce50657
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-07 21:01:26 +0200

    feat: basic content

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Nazar Yavnyy - software engineer";
+export const SITE_DESCRIPTION =
+  "I'm Nazar, a Javascript software engineer that focuses on front end tools such as React and Typescript. My area of expertise is React ecosystem, but I also have experience in backend software development.";
