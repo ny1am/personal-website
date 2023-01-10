fe28ef13079c2afc647befeac2a2d2771c0f1f2b
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-10 09:47:40 +0200

    fix: responsive image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://nazaryy.dev',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
