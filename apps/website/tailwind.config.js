5223d5b7000dec49d5790e2af15b941779d10ce8
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-23 07:01:52 +0200

    chore: type module

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+export default {
+  content: ['./src/**/*.{astro,ts}'],
   theme: {
     extend: {},
   },
