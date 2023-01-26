e5d7c96a09423556c703676c2f6d7bc4c0aa61c8
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-26 08:51:52 +0200

    refactor: astro update fixes

--- apps/website/astro.config.js
+++ apps/website/astro.config.js
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://nazaryy.dev',
+  site: 'https://nazaryy.dev/',
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     image({
