f7ca8ebd7947c7f0d0e71429f54b7dad05c0ddc3
Nazar Yavnyy <nazar.tools@pm.me>
2023-01-28 12:34:09 +0200

    refactor: raw file import

--- libs/resume-theme/src/index.ts
+++ libs/resume-theme/src/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import Handlebars from 'handlebars';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { dateFormatter } from './utils.js';
 
@@ -8,15 +9,15 @@ Handlebars.registerHelper('date', function dateHelper(date: string) {
   return dateFormatter.format(new Date(date));
 });
 
-function getPath(fileName: string) {
-  //TODO: revise
-  return path.resolve(process.cwd(), `../resume-theme/src/${fileName}`);
+function rawFile(fileName: string) {
+  const dir = path.dirname(fileURLToPath(import.meta.url));
+  return fs.readFileSync(path.resolve(dir, `./${fileName}`), 'utf-8');
 }
 
 export default {
   render: (resume: Record<string, unknown>) => {
-    const css = fs.readFileSync(getPath('style.css'), 'utf-8');
-    const template = fs.readFileSync(getPath('resume.hbs'), 'utf-8');
+    const css = rawFile('style.css');
+    const template = rawFile('resume.hbs');
 
     return Handlebars.compile(template)({ css, resume });
   },
