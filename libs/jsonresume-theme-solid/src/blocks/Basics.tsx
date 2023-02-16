be5e412acf680b957ff846e1ce9651d7089c5e38
Nazar Yavnyy <nazar.tools@pm.me>
2023-02-16 19:55:59 +0200

    refactor(jsonresume-theme-solid): guard basics links

--- libs/jsonresume-theme-solid/src/blocks/Basics.tsx
+++ libs/jsonresume-theme-solid/src/blocks/Basics.tsx
@@ -4,14 +4,17 @@ type Props = { basics: Required<ResumeSchema>['basics'] };
 
 export function Basics(props: Props) {
   const basics = props.basics;
+  const hasUrls = Boolean(basics.email || basics.url);
   return (
     <section>
       <header>
         <h1 class="text-3xl font-bold">{basics.name}</h1>
-        <div class="mt-3 flex flex-wrap gap-x-4 gap-y-1 [&>a]:text-sky-600 ">
-          {basics.email && <a href={`mailto:${basics.email}`}>{basics.email}</a>}
-          {basics.url && <a href={basics.url}>{basics.url}</a>}
-        </div>
+        {hasUrls && (
+          <div class="mt-3 flex flex-wrap gap-x-4 gap-y-1 [&>a]:text-sky-600">
+            {basics.email && <a href={`mailto:${basics.email}`}>{basics.email}</a>}
+            {basics.url && <a href={basics.url}>{basics.url}</a>}
+          </div>
+        )}
       </header>
       {basics.summary && <p class="mt-3">{basics.summary}</p>}
     </section>
