cce1498e3c9c19e87f8c90b4f6adcd9b6a5f7291
Nazar Yavnyy <nazar.tools@pm.me>
2023-02-19 19:11:37 +0200

    fix(jsonresume-theme-solid): education section issue

--- libs/jsonresume-theme-solid/src/blocks/Education.tsx
+++ libs/jsonresume-theme-solid/src/blocks/Education.tsx
@@ -10,10 +10,7 @@ export function Education(props: Props) {
         <div class="mb-6 break-inside-avoid">
           <header>{item.institution && <h3 class="font-bold">{item.institution}</h3>}</header>
           <DateRange {...item} />
-          <div class="mt-2">
-            {item.studyType && <span>{item.studyType}</span>}
-            {item.area && <span> - {item.area}</span>}
-          </div>
+          <div class="mt-2">{[item.studyType, item.area].filter(Boolean).join(' - ')}</div>
         </div>
       ))}
     </Section>
